6d38c6532f7ea5581f0bf7a58b15d029f8a2c2de
BlossomBuild <blossombuildLLC@outlook.com>
2025-06-30 14:13:05 -0700

    Completed SocialPost Data, put in dummy data as well

---
+++ BloomBoard/SocialPost.swift
@@ -0,0 +1,70 @@
+//
+//  SocialPost.swift
+//  BloomBoard
+//
+//  Created by Carlos Valentin on 6/30/25.
+//
+
+import Foundation
+
+struct SocialPost: Decodable, Identifiable {
+    var id = UUID().description
+    var title: String?
+    var description: String?
+    var postType: PostType
+    var platform: SocialMediaPlatform
+    var postDay: Day
+    
+    static var skeletonWeekExample: [SocialPost] {
+        [
+            SocialPost(postType: PostType.LongFormVideo, platform: SocialMediaPlatform.Youtube, postDay: Day.Sunday),
+            SocialPost(postType: PostType.ImagePost, platform: SocialMediaPlatform.X, postDay: Day.Sunday),
+            SocialPost(postType: PostType.ImagePost, platform: SocialMediaPlatform.Reddit, postDay: Day.Sunday),
+            
+            SocialPost(postType: PostType.ImagePost, platform: SocialMediaPlatform.Reddit, postDay: Day.Monday),
+            SocialPost(postType: PostType.ImagePost, platform: SocialMediaPlatform.X, postDay: Day.Monday),
+            SocialPost(postType: PostType.ShortFormVideo, platform: SocialMediaPlatform.Youtube, postDay: Day.Monday),
+            
+            SocialPost(postType: PostType.ShortFormVideo, platform: SocialMediaPlatform.Youtube, postDay: Day.Tuesday),
+            SocialPost(postType: PostType.ImagePost, platform: SocialMediaPlatform.X, postDay: Day.Tuesday),
+            
+            
+            SocialPost(postType: PostType.ImagePost, platform: SocialMediaPlatform.Reddit, postDay: Day.Wednesday),
+            SocialPost(postType: PostType.ImagePost, platform: SocialMediaPlatform.X, postDay: Day.Wednesday),
+            SocialPost(postType: PostType.ShortFormVideo, platform: SocialMediaPlatform.Youtube, postDay: Day.Wednesday),
+            
+            
+            SocialPost(postType: PostType.ImagePost, platform: SocialMediaPlatform.X, postDay: Day.Thursday),
+            SocialPost(postType: PostType.QuizPost, platform: SocialMediaPlatform.Youtube, postDay: Day.Thursday),
+            
+            SocialPost(postType: PostType.ImagePost, platform: SocialMediaPlatform.X, postDay: Day.Friday),
+            SocialPost(postType: PostType.ImagePost, platform: SocialMediaPlatform.Reddit, postDay: Day.Friday),
+            
+            SocialPost(postType: PostType.TextPost, platform: SocialMediaPlatform.X, postDay: Day.Saturday)
+        ]
+    }
+}
+
+enum PostType: String, Decodable {
+    case LongFormVideo
+    case ShortFormVideo
+    case ImagePost
+    case QuizPost
+    case TextPost
+}
+
+enum SocialMediaPlatform: String, Decodable {
+    case Reddit
+    case Youtube
+    case X
+}
+
+enum Day: String, Decodable {
+    case Monday
+    case Tuesday
+    case Wednesday
+    case Thursday
+    case Friday
+    case Saturday
+    case Sunday
+}
