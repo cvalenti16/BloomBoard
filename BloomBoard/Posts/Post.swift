a6ea4f10bbb447bdbbf6ba486dbe6294705924f5
BlossomBuild <blossombuildLLC@outlook.com>
2025-08-13 08:59:16 -0700

    Added Performance Enum

--- BloomBoard/Posts/Post.swift
+++ BloomBoard/Posts/Post.swift
@@ -15,16 +15,26 @@ class Post:Identifiable {
     var image:String?
     var postDate: Date?
     var creationDate: Date
+    var performance: Performance?
     
-    init(id: String = UUID().description, title: String, image: String? = nil, completedDate: Date? = nil) {
+    init(id: String = UUID().description, title: String, image: String? = nil, completedDate: Date? = nil, performance: Performance? = nil) {
         self.id = id
         self.title = title
         self.image = image
         self.postDate = completedDate
         self.creationDate = Date()
+        self.performance = performance
+    }
+    
+    enum Performance: String, CaseIterable,Codable {
+        case unrated = "Unrated"
+        case poor = "Poor (1-999 views)"
+        case good = "Good (1K - 4,999 views)"
+        case excellent = "Excellent (5K+ views)"
     }
     
     static var testPost = Post(title: "How is my SwiftData Model?", image: "PostSampleImage")
+
 }
 
 
