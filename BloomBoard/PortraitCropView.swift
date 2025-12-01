3b0ba1c1aa6b9671150d2f0315f1fe31ed542ec0
BlossomBuild <blossombuildLLC@outlook.com>
2025-12-01 10:40:15 -0700

    Added toolbar and navigation title

--- BloomBoard/PortraitCropView.swift
+++ BloomBoard/PortraitCropView.swift
@@ -1,11 +1,12 @@
 import SwiftUI
 
 struct PortraitCropView: View {
-    let post: Post
-    private let targetAspect: CGFloat = 9.0 / 16.0
-    
+    @Environment(\.dismiss) var dismiss
     @State private var offset: CGSize = .zero
     @State private var lastOffset: CGSize = .zero
+
+    private let targetAspect: CGFloat = 9.0 / 16.0
+    let post: Post
     
     var loadedImage: UIImage? {
         guard let data = post.image else { return nil }
@@ -13,34 +14,46 @@ struct PortraitCropView: View {
     }
     
     var body: some View {
-        GeometryReader { geo in
-            let width = geo.size.width
-            let height = width / targetAspect
-            
-            ZStack {
-                if let image = loadedImage {
-                    Image(uiImage: image)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: width, height: height)
-                        .offset(offset)
-                        .gesture(
-                            DragGesture()
-                                .onChanged { value in
-                                    offset = CGSize(
-                                        width: lastOffset.width + value.translation.width,
-                                        height: lastOffset.height + value.translation.height
-                                    )
-                                }
-                                .onEnded { _ in
-                                    lastOffset = offset
-                                }
-                        )
+        NavigationStack {
+            GeometryReader { geo in
+                let width = geo.size.width
+                let height = width / targetAspect
+                
+                ZStack {
+                    if let image = loadedImage {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .offset(offset)
+                            .gesture(
+                                DragGesture()
+                                    .onChanged { value in
+                                        offset = CGSize(
+                                            width: lastOffset.width + value.translation.width,
+                                            height: lastOffset.height + value.translation.height
+                                        )
+                                    }
+                                    .onEnded { _ in
+                                        lastOffset = offset
+                                    }
+                            )
+                    }
+                }
+                .frame(width: width, height: height)
+                .background(Color.black)
+                .position(x: geo.size.width / 2, y: geo.size.height / 2)
+            }
+            .navigationTitle("Convert to 9:16")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: UIIcons.cancel)
+                    }
                 }
             }
-            .frame(width: width, height: height)
-            .background(Color.black)
-            .position(x: geo.size.width / 2, y: geo.size.height / 2)
         }
     }
 }
