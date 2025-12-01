4d44b817451b31d8d656d2e707f8bb445156e9a5
BlossomBuild <blossombuildLLC@outlook.com>
2025-12-01 11:46:37 -0700

    Added labels hidden

--- BloomBoard/PortraitCropView.swift
+++ BloomBoard/PortraitCropView.swift
@@ -98,6 +98,7 @@ struct PortraitCropView: View {
                         selection: $backgroundColor,
                         supportsOpacity: false
                     )
+                    .labelsHidden()
                 }
             }
         }
