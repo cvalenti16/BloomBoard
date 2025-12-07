32756b4bbd8422f6cb52275c7a8a6935c5551c3f
BlossomBuild <blossombuildLLC@outlook.com>
2025-12-07 03:11:22 -0700

    Moved PostItemView to PostListView

--- BloomBoard/PostListView.swift
+++ BloomBoard/PostListView.swift
@@ -117,6 +117,51 @@ private class PostListState {
     var selectedMissingPlatform: SocialMedia? = nil
 }
 
+//MARK: PostItemView
+private struct PostItemView: View {
+    let post: Post
+    let onSelect: (Post) -> Void
+    var hasImage: Bool {
+        post.image != nil
+    }
+    
+    var body: some View {
+        VStack (alignment: .leading) {
+            Button {
+                onSelect(post)
+            } label: {
+                Text(post.title)
+                    .bold()
+                    .font(.title3)
+                    .foregroundStyle(.text)
+                    .lineLimit(2)
+                    .truncationMode(.tail)
+            }
+            
+            HStack {
+                if let postDate = post.postDate {
+                    Text("\(UIStrings.posted)\(postDate, style: .date)")
+                } else {
+                    Text("\(UIStrings.created)\(post.creationDate, style: .date)")
+                }
+                
+                Image(systemName: hasImage ? UIIcons.photo : UIIcons.document)
+            }
+            .font(.subheadline)
+            .foregroundStyle(.secondary)
+            
+            if let postPerformance = post.performance {
+                Text((postPerformance.rawValue))
+                    .padding(5)
+                    .background(postPerformance.color.opacity(0.5))
+                    .clipShape(.rect(cornerRadius: 10))
+                    .font(.subheadline)
+            }
+        }
+    }
+}
+
+
 // MARK: PostDeleteAlert
 private struct PostDeleteAlert: ViewModifier {
     @Environment(\.modelContext) private var modelContext
