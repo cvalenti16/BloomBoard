235b80436f302d9b40fcd979ddb9d93fcc0843c9
BlossomBuild <blossombuildLLC@outlook.com>
2025-12-07 03:57:06 -0700

    Fixed spacing for summary

--- BloomBoard/PostDetailView.swift
+++ BloomBoard/PostDetailView.swift
@@ -159,7 +159,7 @@ private struct SocialMediaSummary: View {
             }
             .font(.footnote)
             .foregroundStyle(.secondary)
-            .padding(.horizontal, 5)
+            .padding(.horizontal)
         }
     }
     
