26c222eae636a3b43d780b17ca2dd27e4db3b918
BlossomBuild <blossombuildLLC@outlook.com>
2025-12-15 11:33:20 -0700

    Added SettingsView

---
+++ BloomBoard/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  BloomBoard
+//
+//  Created by Carlos Valentin on 12/15/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
