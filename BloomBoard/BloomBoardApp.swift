32a03e3dbc5ca8a6c7492c94ce419145426f221e
BlossomBuild <blossombuildLLC@outlook.com>
2026-04-20 14:31:42 -0700

    Added contentview back

--- BloomBoard/BloomBoardApp.swift
+++ BloomBoard/BloomBoardApp.swift
@@ -32,7 +32,7 @@ struct BloomBoardApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PostListView()
                 .preferredColorScheme(selectedAppearance.colorScheme)
         }
         .modelContainer(modelContainer)
