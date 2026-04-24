150b6ca9359a89892c76fb16436858e3b4b95ca2
BlossomBuild <blossombuildLLC@outlook.com>
2026-04-23 21:49:29 -0700

    Changed title and added padding to the button

--- BloomBoard/PostEditorView.swift
+++ BloomBoard/PostEditorView.swift
@@ -69,7 +69,7 @@ struct PostEditorView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                TextField("Title", text: $title, axis: .vertical)
+                TextField("What's new?", text: $title, axis: .vertical)
                     .padding()
                     .font(.title3)
                     .focused($isTitleFocused)
@@ -86,6 +86,7 @@ struct PostEditorView: View {
                             Text(title)
                                 .font(.subheadline)
                                 .foregroundStyle(.text)
+                                .padding()
                         }
                 }
                 
