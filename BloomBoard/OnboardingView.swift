b5d74a1c7a088e9d1c5378c90012e93a75635d09
BlossomBuild <blossombuildLLC@outlook.com>
2026-05-27 20:31:32 -0700

    started onboarded screen

--- BloomBoard/OnboardingView.swift
+++ BloomBoard/OnboardingView.swift
@@ -30,11 +30,53 @@ struct OnboardingView: View {
     
     var body: some View {
         VStack {
-            Spacer()
+            TabView(selection: $currentPage) {
+                WelcomeView()
+                    .tag(OnboardingPage.welcome)
+            }
         }
     }
 }
 
+private struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            AppIcon()
+            
+            Text("Welcome to BloomBoard")
+                .font(.title)
+                .bold()
+            
+            Text("Made to help you write better X posts")
+                .font(.title3)
+                
+        }
+        .padding()
+    }
+}
+
+extension Bundle {
+    public var icon: UIImage? {
+        if let icons = infoDictionary?["CFBundleIcons"] as? [String: Any],
+           let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+           let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+           let lastIcon = iconFiles.last {
+            return UIImage(named: lastIcon)
+        }
+        return nil
+    }
+}
+
+struct AppIcon: View {
+    var body: some View {
+        Image(uiImage: Bundle.main.icon ?? UIImage())
+            .resizable()
+            .scaledToFill()
+            .clipped()
+            .clipShape(.rect(cornerRadius: 22))
+            .frame(width: 100, height: 100)
+    }
+}
 
 #Preview {
     OnboardingView()
